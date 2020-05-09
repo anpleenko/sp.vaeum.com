45a8834f80f5b432b0334cebf71607a7f6c60311
Alexander Anpleenko <avanpleenko@gmail.com>
2020-05-09 16:01:34 +0300

    added config

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+// https://dev.to/cosmvs/tslint-eslint-cra-typescript-prettier-2h40
+// https://gist.github.com/1natsu172/a65a4b45faed2bd3fa74b24163e4256e
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'react-app',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  plugins: ['@typescript-eslint', 'jest', 'jsx-a11y', 'prettier', 'react', 'react-hooks', 'import'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    tsconfigRootDir: '.',
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'react/prop-types': 0,
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
