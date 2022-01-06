ee66a580c8ee5ba235af53a0bca4e4907a84beab
Alexander Anpleenko <avanpleenko@gmail.com>
2022-01-07 02:03:35 +0300

    react-script to vite

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{ts,tsx}': ['eslint --fix --ext .js,.jsx,.ts,.tsx', 'prettier --write'],
   '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged'],
   'package.json': ['sort-package-json', 'prettier --write'],
 };
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,11 @@
 import { FC } from 'react';
+import { Reboot } from 'styled-reboot';
 
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
 import { LinksList } from 'components/LinksList';
 import { contentLinks } from 'data/contentLinks';
 import { popularLinks } from 'data/popularLinks';
-import 'css.modifiers';
-import 'bootstrap/dist/css/bootstrap-reboot.css';
-import 'bootstrap/dist/css/bootstrap-grid.css';
 import { GlobalStyles } from 'globalStyles';
 
 export const App: FC = () => {
@@ -15,6 +13,7 @@ export const App: FC = () => {
 
   return (
     <>
+      <Reboot />
       <GlobalStyles />
       <Header />
       <div className="container-fluid main pl0 pr0" id="main" data-testid="appMainBlock">
